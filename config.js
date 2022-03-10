aac7e68184c21af25e3fabd999f7350b1fe46ee7
Ajimiko4 <84181133+Ajimiko4@users.noreply.github.com>
2022-03-10 21:34:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 
 let fs = require('fs')
-global.owner = ['6282221xxxxxx', '6282221xxxxxx','6282221xxxxxx', '6282221xxxxxx', '6282221xxxxxx'] // Letakan nomor kamu disini [put your number here bro]
+global.owner = ['6281232444391', '6281232444391','6281232444391', '6281232444391', '6281232444391'] // Letakan nomor kamu disini [put your number here bro]
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
@@ -57,7 +57,7 @@ global.kandang = 'https://telegra.ph/file/67a6ee607d03a4e52757d.jpg'
 global.kolam = 'https://telegra.ph/file/5aa5dfa3394477e11fb18.jpg'
 global.thanks = 'https://telegra.ph/file/01917f7782b70de8f418d.jpg'
 
-global.multiplier = 9999999 // Semakin tinggi, semakin sulit naik level
+global.multiplier = 30 // Semakin tinggi, semakin sulit naik level
 
 let chalk = require('chalk')
 let file = require.resolve(__filename)
